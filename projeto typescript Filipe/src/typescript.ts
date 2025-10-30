0cb471e27ac5fa9fb1cfef7dfadc967d6f4e65a0
Filipemsousa <filipmenzessousa@gmail.com>
2025-10-30 11:43:59 -0300

    remocao de pastas extras

--- projeto typescript Filipe/src/typescript.ts
+++
@@ -1,9 +0,0 @@
-let valor: number = 3000;
-let nome: string = "";
-let isPago: boolean = false;
-let qualquer: any = "";
-qualquer = 22;
-
-//Arrays
-const lista: number[] = [];
-lista.push(13,22.5,22,89,1.58);
