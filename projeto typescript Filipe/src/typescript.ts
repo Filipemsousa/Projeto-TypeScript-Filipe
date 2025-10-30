efce7884e5c0900ee3769e74282c7650fff5ec98
Filipemsousa <filipmenzessousa@gmail.com>
2025-10-30 10:27:50 -0300

    adicao de pastas

---
+++ projeto typescript Filipe/src/typescript.ts
@@ -0,0 +1,9 @@
+let valor: number = 3000;
+let nome: string = "";
+let isPago: boolean = false;
+let qualquer: any = "";
+qualquer = 22;
+
+//Arrays
+const lista: number[] = [];
+lista.push(13,22.5,22,89,1.58);
